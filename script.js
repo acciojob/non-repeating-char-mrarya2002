8c9e68167a3842b4f3324dbf8549aae1447e2197
Nikhil Arya <nikhilarya3108202@gmail.com>
2025-08-19 17:43:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+// let arr = [];
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 const charCount = new Array(256).fill(0);
+        
+            // First pass: count all characters using their ASCII codes
+            for (let i = 0; i < str.length; i++) {
+                const charCode = str.charCodeAt(i);
+                charCount[charCode]++;
+            }
+            
+            // Second pass: find the first character with count of 1
+            for (let i = 0; i < str.length; i++) {
+                const charCode = str.charCodeAt(i);
+                if (charCount[charCode] === 1) {
+                    return str[i];
+                }
+            }
+            
+            // If no non-repeating character found
+            return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
